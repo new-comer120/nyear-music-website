f0833d87afb0fd273dc470f0adbdf550bfce071f
Nam <toymyorange@gmail.com>
2021-09-17 20:00:40 +0700

    First Pass

---
+++ music-list.js
@@ -0,0 +1,57 @@
+let allMusic = [
+  {
+    name: "OMORI - Final Duet",
+    artist: "OMOCAT",
+    img: "final-duet",
+    src: "OMORI - Final Duet"
+  },
+  {
+    name: "(Playing On Muse Dash) Hai Phút Hơn (Trance Ảo Giác Remix)",
+    artist: "Unknown",
+    img: "hai-phut-hon",
+    src: "(Playing On Muse Dash) Hai Phút Hơn (Trance Ảo Giác Remix)"
+  },
+  {
+    name: "bo en - my time",
+    artist: "bo en",
+    img: "my-time",
+    src: "bo en - my time"
+  },
+  {
+    name: "Friday Night Funkin  Maginage Matches Mod - Full Soundtrack, Tic Tac Foe, Hit-N Strike, Acrimony",
+    artist: "VeroBeats",
+    img: "maginage-matches",
+    src: "Friday Night Funkin  Maginage Matches Mod - Full Soundtrack, Tic Tac Foe, Hit-N Strike, Acrimony"
+  },
+  {
+    name: "Hãy Trao Cho Anh - Miku Cover",
+    artist: "Hatsune Miku",
+    img: "hay-trao-cho-anh",
+    src: "Hãy Trao Cho Anh - Miku Cover"
+  },
+  {
+    name: "Jaiden Animations - Rise Above (feat. Rainych)",
+    artist: "JaidenAnimations",
+    img: "rise-above",
+    src: "Jaiden Animations - Rise Above (feat. Rainych)"
+  },
+  {
+    name: "Raiden Shogun Theme (Judgment of Euthymia) - Genshin Impact",
+    artist: "HOYO-MiX",
+    img: "raiden-shogun-theme",
+    src: "Raiden Shogun Theme (Judgment of Euthymia) - Genshin Impact"
+  },
+  {
+    name: "Thích Thì Đến - Lê Bảo Bình",
+    artist: "Lê Bảo Bình",
+    img: "thich-thi-den",
+    src: "Thích Thì Đến - Lê Bảo Bình"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
