a24c12fa5578926ac25e63aed1e2773bbdcf32b2
Nam <toymyorange@gmail.com>
2021-10-23 07:12:01 +0700

    ended trial

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // Select all the elements in the HTML page
 // and assign them to a variable
 let now_playing = document.querySelector(".now-playing");
@@ -241,3 +242,22 @@ if (!isNaN(curr_track.duration)) {
 
 // Load the first track in the tracklist
 loadTrack(track_index);
+=======
+const countdown = () => {
+	const countDate = new Date("December 30, 2021 20:30:00").getTime();
+	const now = new Date().getTime();
+	const gap = countDate - now;
+
+	const textDay = Math.floor(gap / (1000 * 60 * 60 * 24));
+	const textHour = Math.floor((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	const textMinute = Math.floor((gap % (1000 * 60 * 60)) / (1000 * 60));
+	const textSecond = Math.floor((gap % (1000 * 60)) / 1000);
+
+	document.querySelector(".day").innerText = textDay;
+	document.querySelector(".hour").innerText = textHour;
+	document.querySelector(".minute").innerText = textMinute;
+	document.querySelector(".second").innerText = textSecond;	
+};
+
+setInterval(countdown, 1000);
+>>>>>>> Stashed changes
